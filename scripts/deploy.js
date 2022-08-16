4f7b8a1adfc21c853ea420745b97a82a8fca5b63
abc <swati@solulab.com>
2022-08-16 11:52:02 +0530

    Added deployment script for LazyMinting

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require('hardhat')
 
 async function main() {
@@ -13,11 +7,9 @@ async function main() {
 	const lazyMinting = await LazyMinting.deploy()
 	await lazyMinting.deployed()
 
-	console.log('lazy Minting address:', lazyMinting.address)
+	console.log('Lazy minting contract address', lazyMinting.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
 	console.error(error)
 	process.exitCode = 1
